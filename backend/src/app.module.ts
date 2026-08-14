4558365d1986922fa102d533b47cde5f1f673f40
Manas <manaskalamkar910@gmail.com>
2026-08-14 15:00:22 +0530

    feat: register application modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { TaskMembersModule } from './task-members/task-members.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule],
+  imports: [PrismaModule, ProjectsModule, TasksModule, UsersModule, CommentsModule, TaskMembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
