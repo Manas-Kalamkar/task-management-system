3d501021abe3ae1ce74f3a6090daa482b83717a1
Manas <manaskalamkar910@gmail.com>
2026-08-17 13:47:39 +0530

    feat: implement JWT authentication

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(3)
+  username!: string;
+
+  @IsString()
+  @MinLength(2)
+  fullName!: string;
+
+  @IsString()
+  @MinLength(2)
+  title!: string;
+
+  @IsString()
+  @MinLength(6)
+  password!: string;
+}
