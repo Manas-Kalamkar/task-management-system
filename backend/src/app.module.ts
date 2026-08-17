ca52997275f266e19cbe1592a7ce41194a0f0ec8
Manas <manaskalamkar910@gmail.com>
2026-08-17 14:00:19 +0530

    feat: register application modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectsModule } from './projects/projects.module';
-import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 import { CommentsModule } from './comments/comments.module';
 import { TaskMembersModule } from './task-members/task-members.module';
+import { LabelsModule } from './labels/labels.module';
+import { ResourcesModule } from './resources/resources.module';
+import { TaskUpdatesModule } from './task-updates/task-updates.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule, TasksModule, UsersModule, CommentsModule, TaskMembersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    PrismaModule,
+    ProjectsModule,
+    UsersModule,
+    TasksModule,
+    CommentsModule,
+    TaskMembersModule,
+    LabelsModule,
+    ResourcesModule,
+    TaskUpdatesModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
