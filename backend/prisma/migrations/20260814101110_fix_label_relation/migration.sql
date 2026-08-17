2172e2cac0deae9eecab35e10849a8a060cf3eac
Manas <manaskalamkar910@gmail.com>
2026-08-17 13:44:34 +0530

    feat: update database schema and migrations

---
+++ backend/prisma/migrations/20260814101110_fix_label_relation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `taskid` on the `Label` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Label" DROP COLUMN "taskid";
